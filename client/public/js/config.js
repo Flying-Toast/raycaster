e38ffe4292964e9f57387cf9cb518262c0b3697c
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-03-22 17:39:40 -0400

    2d map drawing

---
+++ client/public/js/config.js
@@ -0,0 +1,9 @@
+export const cfg = {
+	resolution: { width: 750, height: 400 },
+	tileSize: 15,
+	colors: {
+		wall: "#4D4D4D",
+		//floor color:
+		air: "#D3D0D0"
+	}
+};
--- client/public/js/render.js
+++ client/public/js/render.js
@@ -1,10 +1,22 @@
+import { cfg } from "./config.js";
+
 export class Renderer {
 	constructor(canvas) {
+		this.width = cfg.resolution.width;
+		this.height = cfg.resolution.height;
+		canvas.width = this.width;
+		canvas.height = this.height;
 		this.canvas = canvas;
 		this.ctx = canvas.getContext("2d");
 	}
 
 	render(game) {
-
+		this.ctx.clearRect(0, 0, this.width, this.height);
+		for (const row of game.map.tiles) {
+			for (const t of row) {
+				this.ctx.fillStyle = cfg.colors[t.type];
+				this.ctx.fillRect(t.location.x * cfg.tileSize, t.location.y * cfg.tileSize, cfg.tileSize, cfg.tileSize);
+			}
+		}
 	}
 }
