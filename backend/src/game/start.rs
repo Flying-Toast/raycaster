1ed9efb59b16a8301b8934bc650b99695c0a960d
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-06-20 15:58:23 -0400

    map stuff

--- backend/src/game/start.rs
+++ backend/src/game/start.rs
@@ -8,7 +8,7 @@ use crate::game::map::Map;
 /// Spawns a game thread.
 /// `rx` is the receiver from the networking thread.
 pub fn run_game(rx: Receiver<NetMessage>) {
-    let default_map = Map::from_file("../maps/default.map").unwrap();
+    let default_map = Map::from_file("../maps/default").unwrap();
 
     thread::spawn(move || {
         let mut game = Game::new(rx, default_map);
