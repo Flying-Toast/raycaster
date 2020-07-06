632179ba926c0a3dd66eb00eb9e94143689c2672
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-07-05 21:15:06 -0400

    NetEvent processing

--- backend/src/game/game.rs
+++ backend/src/game/game.rs
@@ -1,6 +1,7 @@
-use std::sync::mpsc::Receiver;
-use crate::net::NetEvent;
+use std::sync::mpsc::{Receiver, TryRecvError};
+use crate::net::{NetEvent, Responder};
 use crate::game::map::Map;
+use crate::protocol::ClientMessage;
 
 
 pub struct Game {
@@ -17,10 +18,42 @@ impl Game {
         }
     }
 
+    /// Called when a client disconnects
+    fn on_client_disconnect(&mut self, connection_id: u32) {
+
+    }
+
+    /// Called when a client connects
+    fn on_client_connect(&mut self, connection_id: u32, responder: Responder) {
+
+    }
+
+    /// Called for each message received from a client
+    fn on_client_message(&mut self, connection_id: u32, message: ClientMessage) {
+
+    }
+
+    /// Receives and processes pending NetEvents.
+    /// Processes a maximum of 500 events each call.
+    fn process_net_events(&mut self) {
+        const MAX_EVENTS: u32 = 500;
+        for _ in 0..MAX_EVENTS {
+            match self.rx.try_recv() {
+                Ok(event) => match event {
+                                 NetEvent::Connect(id, responder) => self.on_client_connect(id, responder),
+                                 NetEvent::Disconnect(id) => self.on_client_disconnect(id),
+                                 NetEvent::Message(id, message) => self.on_client_message(id, message),
+                             },
+                Err(TryRecvError::Empty) => return,
+                Err(TryRecvError::Disconnected) => panic!("Network channel disconnected"),
+            }
+        }
+    }
+
     /// Starts the game loop.
     pub fn run(&mut self) -> ! {
         loop {
-
+            self.process_net_events();
         }
     }
 }
