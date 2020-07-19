47602188b4ab06525ca8c085c0b7eb4ca1e02a43
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-07-19 17:08:15 -0400

    use u32 for string length because javascript

--- backend/src/protocol/payload.rs
+++ backend/src/protocol/payload.rs
@@ -19,7 +19,7 @@ impl Pieces {
 
     /// Parse the next `String`
     pub fn get_string(&mut self) -> Result<String, RCE> {
-        let string_len = self.get_u64()?;
+        let string_len = self.get_u32()?;
         let bytes = self.bytes_from_front(string_len as usize)?;
         let string = String::from_utf8(bytes).to(RCE::BadString)?;
 
@@ -38,16 +38,6 @@ impl Pieces {
 
     pub fn get_u16(&mut self) -> Result<u16, RCE> {
         type Int = u16;
-        Ok(Int::from_be_bytes(
-            self.bytes_from_front(mem::size_of::<Int>())?
-                .as_slice()
-                .try_into()
-                .unwrap()
-        ))
-    }
-
-    pub fn get_u64(&mut self) -> Result<u64, RCE> {
-        type Int = u64;
         Ok(Int::from_be_bytes(
             self.bytes_from_front(mem::size_of::<Int>())?
                 .as_slice()
@@ -95,16 +85,12 @@ impl PayloadBuilder {
     }
 
     pub fn add_str(&mut self, string: &str) {
-        let str_len = string.len() as u64;
-        self.add_u64(str_len);
+        let str_len = string.len() as u32;
+        self.add_u32(str_len);
         self.bytes.extend_from_slice(string.as_bytes());
     }
 
     pub fn add_u32(&mut self, int: u32) {
-        self.bytes.extend_from_slice(&int.to_be_bytes());
-    }
-
-    pub fn add_u64(&mut self, int: u64) {
         self.bytes.extend_from_slice(&int.to_be_bytes());
     }
 
