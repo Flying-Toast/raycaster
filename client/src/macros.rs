91c8706ab802043216ce69556a55ba5b61309dc9
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-08-15 18:32:10 -0400

    shadow prelude println

--- client/src/macros.rs
+++ client/src/macros.rs
@@ -9,3 +9,9 @@ macro_rules! console_error {
         crate::bindings::error(&format_args!($($t)*).to_string())
     };
 }
+
+macro_rules! println {
+    ($($t:tt)*) => {
+        compile_error!("Don't use println in wasm (use console_log)");
+    }
+}
