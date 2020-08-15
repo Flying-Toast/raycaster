5058d7fa57f567490f607f9d927149007ec5012e
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-08-14 23:44:12 -0400

    wasm

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,18 +1,11 @@
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use web_sys::{
-    WebSocket, MessageEvent,
-};
-use common::protocol::payloads::HelloPayload;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
 
 #[wasm_bindgen(start)]
 pub fn start() {
-    let ws = WebSocket::new("ws://localhost:8000").unwrap();
-    let ws2 = ws.clone();
-    let onopen_callback = Closure::wrap(Box::new(move |_| {
-        let q = HelloPayload::assemble("👋Hello from the client!", 39);
-        ws2.send_with_u8_array(q.encode()).unwrap();
-    }) as Box<dyn FnMut(MessageEvent)>);
-    ws.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
-    onopen_callback.forget();
+    alert("HELLO HELLO HELLO HELLO");
 }
