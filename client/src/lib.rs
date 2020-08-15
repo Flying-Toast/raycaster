15113e8025b4152aff96d944016e63aaeae68211
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-08-15 16:53:52 -0400

    console panic hook

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -8,6 +8,8 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
 pub fn start() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
     let f = frontend::Frontend::new();
     f.start();
 }
