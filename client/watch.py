d2defd07b9bbe5daba9527c235ca58d5174152fe
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-08-15 18:48:16 -0400

    Filewatcher doesnt serve files while compiling

--- client/watch.py
+++ client/watch.py
@@ -14,10 +14,12 @@ serve_dir = "static"
 
 def build_wasm():
     print("===== BUILDING WASM =====")
+    LockableRequestHandler.lock = True
     result = subprocess.run(["wasm-pack", "build", "--target", "web"])
     if result.returncode == 0:
         os.replace("pkg/client_bg.wasm", f"{serve_dir}/client_bg.wasm")
         os.replace("pkg/client.js", f"{serve_dir}/client.js")
+        LockableRequestHandler.lock = False
     else:
         print("===== ERROR BUILDING WASM =====")
 
@@ -28,9 +30,16 @@ def watch():
         watch_files=["Cargo.toml", "../common/Cargo.toml"]
     )
 
+class LockableRequestHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if not LockableRequestHandler.lock:
+            super().do_GET()
+
+LockableRequestHandler.lock = False
+
 if __name__ == "__main__":
     watcher = Thread(target=watch, daemon=True)
     watcher.start()
-    Handler = partial(SimpleHTTPRequestHandler, directory=serve_dir)
+    Handler = partial(LockableRequestHandler, directory=serve_dir)
     server = HTTPServer(("", port), Handler)
     server.serve_forever()
