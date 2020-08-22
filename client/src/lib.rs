cc46367f03d559b4fe4fe114742b32616b05d5bc
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-08-21 22:08:12 -0400

    refactor panic hook

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -7,13 +7,11 @@ mod game;
 use wasm_bindgen::prelude::*;
 
 
-fn panic_hook(info: &std::panic::PanicInfo) {
-    console_error!("{}", info);
-}
-
 #[wasm_bindgen(start)]
 pub fn start() {
-    std::panic::set_hook(Box::new(panic_hook));
+    std::panic::set_hook(Box::new(|info| {
+        console_error!("{}", info);
+    }));
 
     let f = frontend::Frontend::new();
     f.start();
