53c8f7756db070413296023d690e88f965ba2680
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2020-08-21 22:04:56 -0400

    abort on panic

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -7,9 +7,13 @@ mod game;
 use wasm_bindgen::prelude::*;
 
 
+fn panic_hook(info: &std::panic::PanicInfo) {
+    console_error!("{}", info);
+}
+
 #[wasm_bindgen(start)]
 pub fn start() {
-    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    std::panic::set_hook(Box::new(panic_hook));
 
     let f = frontend::Frontend::new();
     f.start();
